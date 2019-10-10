e37077e038446e629f6b75e7f57463e6ad99a923
gofocus <go_focus@live.cn>
2019-10-10 17:36:31 +0800

    webpack-dev-server / HtmlWebpackPlugin

--- webpack-study/webpack.config.js
+++ webpack-study/webpack.config.js
@@ -8,18 +8,24 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
-        contentBase: './dist',
+        // 配置访问路径，会从这个路径找index.html
+        contentBase: 'dist',
+        // contentBase: path.join(__dirname, "dist"),
         hot: true,
         port: 3000,
-        open: true
+        open: true,
+        // 配置bundle files的访问路径(硬盘中并没有这个文件)
+        // TODO 配置devServer.publicPath会导致无法自动打开index.html(可以手动访问)
+        // publicPath: "/dist/"
     },
-    plugins: [
-        new htmlWebpackPlugin({
-            template: 'dist/index22.html',
-            // TODO 名字不一样无效
-            // filename: 'index2.html'
-        })
-    ]
+     plugins: [
+         new htmlWebpackPlugin({
+             template: 'src/index.html',
+             // 1.不配置filename，默认为index
+             // 2.生成的html的位置是基于contentBase的
+             // filename: 'index.html'
+         })
+     ]
 };
 
 
