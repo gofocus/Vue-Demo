6abb7500bfd2f1fe21347dd63ad02ff954e41fd2
gofocus <go_focus@live.cn>
2019-10-10 09:26:47 +0800

    ~

---
+++ webpack-study/src/index.js
@@ -0,0 +1,27 @@
+
+
+import _ from 'lodash'
+
+import $ from 'jquery'
+
+
+
+$(function () {
+    $('li:odd').css('backgroundColor', 'lightgreen');
+    $('li:even').css('backgroundColor',function () {
+        return '#' + 'D97364';
+    })
+
+    document.body.appendChild(component());
+
+})
+
+function component() {
+    const element = document.createElement('div');
+
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
