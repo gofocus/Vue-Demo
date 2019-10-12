529e697513f413cc72160df50005a24baee8328b
gofocus <go_focus@live.cn>
2019-10-12 09:16:14 +0800

    轮播图 |vue cms

--- vue-cms/src/components/tabbar/home.vue
+++ vue-cms/src/components/tabbar/home.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        Home
+        <mt-swipe :auto="4000">
+            <mt-swipe-item>1</mt-swipe-item>
+            <mt-swipe-item>2</mt-swipe-item>
+            <mt-swipe-item>3</mt-swipe-item>
+            <mt-swipe-item>4</mt-swipe-item>
+        </mt-swipe>
     </div>
 </template>
 
@@ -10,6 +15,20 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .mint-swipe {
+        height: 200px;
 
+        .mint-swipe-item {
+            &:nth-child(1) {
+                background-color: blue;
+            }
+            &:nth-child(2) {
+                background-color: greenyellow;
+            }
+            &:nth-child(3) {
+                background-color: pink;
+            }
+        }
+    }
 </style>
--- vue-cms/src/index.js
+++ vue-cms/src/index.js
@@ -7,7 +7,10 @@ import 'mint-ui/lib/style.css'
 import './lib/mui/css/mui.min.css';
 import './lib/mui/css/icons-extra.css';
 import './lib/mui/fonts/mui-icons-extra.ttf';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, axios)
 Vue.use(VueRouter);
 Vue.use(MintUI);
 
