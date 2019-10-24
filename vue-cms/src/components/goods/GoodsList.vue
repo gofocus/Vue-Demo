45ba363ba2d1b194e1366e1b6cacfd07b538b308
gofocus <go_focus@live.cn>
2019-10-24 09:33:13 +0800

    goodsList |vue cms

--- vue-cms/src/components/goods/GoodsList.vue
+++ vue-cms/src/components/goods/GoodsList.vue
@@ -1,27 +1,90 @@
 <template>
     <div>
-        <p>goods</p>
+        <div class="goods-list-container">
+            <div class="goods-item">
+                <img src="https://m.360buyimg.com/babel/jfs/t1/51796/3/3090/165250/5d0da795Eab1d07e8/780a99bc93e0230b.png"
+                     alt="">
+                <h1>小米（Mi）小米Note 16G双网通版</h1>
+                <div class="info">
+                    <p class="price">
+                        <span class="now">￥899</span>
+                        <span class="old">￥999</span>
+                    </p>
+                    <p class="sell">
+                        <span>热卖中</span>
+                        <span>剩60件</span>
+                    </p>
+                </div>
+            </div>
+            <div class="goods-item">
+                <img src="//m.360buyimg.com/babel/jfs/t1/52299/20/12979/274134/5d9d9fdeE71d91646/1b4dedd95e4c32fb.jpg!q70.jpg"
+                     alt="">
+                <h1>努比亚 nubia 红魔3S电竞游戏手机 8GB+128GB 玄铁黑 骁龙855Plus UFS3.0 内置风扇 5000mA</h1>
+                <div class="info">
+                    <p class="price">
+                        <span class="now">￥899</span>
+                        <span class="old">￥999</span>
+                    </p>
+                    <p class="sell">
+                        <span>热卖中</span>
+                        <span>剩60件</span>
+                    </p>
+                </div>
+            </div>
+            <div class="goods-item">
+                <img src="//m.360buyimg.com/babel/jfs/t1/67822/22/9911/141808/5d7804a3E1c1796ce/a4ff0dd48086f3e4.jpg!q70.jpg"
+                     alt="">
+                <h1>小米（Mi）小米Note 16G双网通版</h1>
+                <div class="info">
+                    <p class="price">
+                        <span class="now">￥899</span>
+                        <span class="old">￥999</span>
+                    </p>
+                    <p class="sell">
+                        <span>热卖中</span>
+                        <span>剩60件</span>
+                    </p>
+                </div>
+            </div>
+
+
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: "GoodsList",
-        data(){
-          return {
+        data() {
+            return {
 
-          }
+            }
         },
-        methods:{
+        methods: {
 
         },
-        created(){
+        created() {
 
         },
 
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .goods-list-container {
+
+
+        .goods-item {
+
+            img {
+                width: 49%;
+            }
+
+
+
+
+        }
+
+    }
 
 </style>
