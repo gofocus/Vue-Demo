17cd3685fee84ba73258de897dd89feaf4144562
gofocus <go_focus@live.cn>
2019-11-22 17:48:27 +0800

    动画、轮播图 | JavaScript

---
+++ JavaScript/BOM/animate.js
@@ -0,0 +1,19 @@
+function animate(obj, target, callback) {
+    // 防止反复点击按钮，先清除以前的定时器，只保留当前的一个定时器执行
+    clearInterval(obj.timer);
+
+    obj.timer = setInterval(function () {
+
+        if (obj.offsetLeft === target) {
+            clearInterval(obj.timer);
+            if (callback) {
+                callback();
+            }
+        }
+        let step = (target - obj.offsetLeft) / 10;
+        step = step > 0 ? Math.ceil(step) : Math.floor(step);
+        obj.style.left = obj.offsetLeft + step + 'px';
+
+    }, 15)
+
+}
