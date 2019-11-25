b74bd1d2d3e5c0ea5dc859ac063e99ea51d5d1d2
gofocus <go_focus@live.cn>
2019-11-25 16:56:15 +0800

    面向对象tab栏 删除功能模块 | JavaScript OOP

--- JavaScript/OOP/js/02面向对象案例.js
+++ JavaScript/OOP/js/02面向对象案例.js
@@ -3,20 +3,19 @@ let _this;
 class Tab {
     constructor() {
         this.main = document.querySelector('ul');
-        this.lis = document.querySelectorAll('li');
         this.section = document.querySelector('.tabscon');
         this.tabadd = document.querySelector('.tabadd');
-        this.sections = document.querySelectorAll('section');
         _this = this;
         this.init();
     }
 
     init() {
+        this.lis = document.querySelectorAll('li');
+        this.sections = document.querySelectorAll('section');
+        this.icons = document.querySelectorAll('.icon-guanbi');
+
         for (let i = 0; i < this.lis.length; i++) {
             this.lis[i].index = i;
-
-            // toggleTab不能加(),否则会直接调用
-            // this.lis[i].onclick = this.toggleTab();
             this.lis[i].onclick = this.toggleTab;
             /*
                             // TODO 如果要传参怎么办？
@@ -25,11 +24,14 @@ class Tab {
                                 _this.toggleTab(i);
                             }
             */
+
+            // ×图标绑定删除事件
+            this.icons[i].onclick = this.removeTab;
         }
 
         // +号按钮绑定点击事件添加tab
-        this.tabadd.addEventListener('click', this.addTab);
-        // this.tabadd.onclick = this.addTab;
+        this.tabadd.onclick = this.addTab;
+
 
     }
 
@@ -50,25 +52,32 @@ class Tab {
              _this.main.insertAdjacentHTML('beforeend', newTab);*/
         // 用insertAdjacentHTML不能赋值
         let newTab = document.createElement('li');
-        newTab.innerHTML = `<span>newTab</span><span class="iconfont icon-guanbi"></span>`;
+        newTab.innerHTML = `<span>newTab${_this.lis.length + 1}</span><span class="iconfont icon-guanbi"></span>`;
         newTab.index = _this.lis.length;
-        // _this.main.appendChild(newTab);  // 用insertAdjacentElement可以灵活的选择插入位置
         _this.main.insertAdjacentElement('beforeend', newTab);
-        _this.lis = document.querySelectorAll('li');
-        newTab.addEventListener('click', _this.toggleTab);
+        // newTab.addEventListener('click', _this.toggleTab);   // 要初始化的东西可能有多个，调用init函数来初始化
 
         //插入section
         let newSection = document.createElement('section');
-        newSection.innerHTML = 'New Tab content' + Math.random();
-        // _this.section.appendChild(newSection);
+        newSection.innerHTML = 'New Tab' + (_this.lis.length + 1);
         _this.section.insertAdjacentElement('beforeend', newSection);
-        _this.sections = document.querySelectorAll('section');
 
+        _this.init();
         newTab.click()  //手动调用toggleTab，将新创建的tab设置为当前选中的tab
     }
 
-    removeTab() {
-
+    removeTab(e) {
+        e.stopPropagation();
+        let index = this.parentNode.index;
+        // _this.main.removeChild(_this.lis[index]);
+        _this.lis[index].remove();  // 实验API
+        _this.sections[index].remove();
+        _this.init();
+        if (document.querySelector('.liactive')) {
+            return;     // 删除active选项卡，才需要设置样式
+        }
+        index = index === 0 ? 0 : --index;
+        _this.lis[index] && _this.lis[index].click(); // 设置样式为liactive
     }
 
     editTab() {
